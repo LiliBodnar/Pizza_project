d33d824a3d794811e842dc4f16e984f1fc0bb9c2
bruna <b.cavalcantilauro@student.maastrichtuniversity.nl>
2024-10-14 23:00:40 +0200

    final final final final

--- OrderProcessing.py
+++ OrderProcessing.py
@@ -2,7 +2,6 @@ from datetime import datetime, timedelta
 import os
 import time
 import threading
-from typing import Self
 from Menu import display_menu
 from AccountManagement import AccountManagement
 from DeliveryManagement import DeliveryManagement
